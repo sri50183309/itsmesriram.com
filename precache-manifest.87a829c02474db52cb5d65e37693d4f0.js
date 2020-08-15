15557546ad67ec65305ed59ee388e1c9839aef46
tstone10 <tstone10@MGC15Y606EJ1GP.fbpld77.ford.com>
2020-08-15 08:40:47 -0400

    Updates

---
+++ precache-manifest.87a829c02474db52cb5d65e37693d4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1503aa8889a20b11f6991abac1942f20",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "c71ae6b3d345d96e9991",
+    "url": "/portfolio-create-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "77473fff947d47a82a51",
+    "url": "/portfolio-create-react-app/static/js/2.c8ae445c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-create-react-app/static/js/2.c8ae445c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71ae6b3d345d96e9991",
+    "url": "/portfolio-create-react-app/static/js/main.8374b777.chunk.js"
+  },
+  {
+    "revision": "f2b8295b299fac858656",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.08eb8b6c.js"
+  }
+]);
